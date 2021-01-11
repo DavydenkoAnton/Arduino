be8eddd842f9906edb63791a018efdbf0d5e3e91
DavydenkoAnton <minskdevelopment@gmail.com>
2021-01-11 05:29:32 +0300

    add led strip

--- eva/points/panchenko/smallRoom/nightLight_1/smallRoomNightLight_1.ino
+++ eva/points/panchenko/smallRoom/nightLight_1/smallRoomNightLight_1.ino
@@ -1,12 +1,21 @@
 #include <ESP8266WiFi.h>        // wifi
 #include <FirebaseESP8266.h>    // firebase
+#include <Adafruit_NeoPixel.h>  // WS212B
 
 const char* WIFI_SSID = "Salva";
 const char* WIFI_PASSWORD = "!Panchenko!60";
 #define FIREBASE_HOST "panchenko-db04d.firebaseio.com"
 #define FIREBASE_AUTH "Sj4vGGLFRnMq63hYPS2kVCSAnTlk7dZ50NxwP4IH"
-
+#define NUM_LEDS    5           // Number of LEDs connected.
+int pirPin = D1;
+int ledPin = D4;
+String lightMode = "static";
+String color = "";
+int brightness = 100;
+bool connection = false;
 FirebaseData firebaseData;
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LEDS, ledPin, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   Serial.begin(115200); //enable Serial Monitor
@@ -14,20 +23,65 @@ void setup() {
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   for (int i = 0; i < 5; i++) {
     if (WiFi.status()) {
-      Serial.println("wifi connected");
+      connection = true;
       break;
     }
+    delay(100);
+  }
+  if (!connection) {
+    delay(5000);
+    ESP.restart();
   }
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   Firebase.reconnectWiFi(true);
-  //pinMode(kitchenController.kitchenWindowPirPin, INPUT);  // pir pin
+
+  pinMode(pirPin, INPUT);   // pir pin
+  pinMode(ledPin, OUTPUT);  // led pin
+
+  pixels.begin();
 }
 
 void loop() {
-  if (Firebase.setString(firebaseData, "panchenko/smallRoom/light/nightLight_1/status", "on")) {
-    Serial.println("failed");
-  } else {
-    Serial.println("sended");
+  getLightMode();
+  getBrightness();
+
+  if (lightMode.equals("static")) {
+    for (int i = 0; i < NUM_LEDS; i++) {
+      pixels.setPixelColor(i, pixels.Color(0, 255, 0));
+      pixels.show();
+      delay(1000);
+    }
+  } else if (lightMode.equals("auto")) {
+    for (int i = 0; i < NUM_LEDS; i++) {
+      pixels.setPixelColor(i, pixels.Color(255, 0, 0));
+      pixels.show();
+      delay(1000);
+    }
   }
-  delay(5000);
+
+  runLed();
+  delay(3000);
 }
+
+void runLed() {
+
+}
+
+void getLightMode() {
+  if (Firebase.getString(firebaseData, "panchenko/smallRoom/light/nightLight_1/lightMode")) {
+    lightMode = firebaseData.stringData();
+  }
+}
+
+void getColor() {
+  if (Firebase.getString(firebaseData, "panchenko/smallRoom/light/nightLight_1/color")) {
+    color = firebaseData.stringData();
+  }
+}
+
+void getBrightness() {
+  if (Firebase.getInt(firebaseData, "panchenko/smallRoom/light/nightLight_1/brightness")) {
+    brightness = firebaseData.intData();
+  }
+}
+
