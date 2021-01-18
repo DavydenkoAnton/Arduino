a1f9a0aa42f2e36c1a3734c64baba60368112267
DavydenkoAnton <minskdevelopment@gmail.com>
2021-01-18 03:13:26 +0300

    Update smallRoomNightLight_1.ino

--- eva/points/panchenko/smallRoom/nightLight_1/smallRoomNightLight_1.ino
+++ eva/points/panchenko/smallRoom/nightLight_1/smallRoomNightLight_1.ino
@@ -13,18 +13,28 @@ int ledPin = D4;
 String ledStripMode = "static";
 int brightness = 150;
 int red = 0;
-int green = 0;
+int green = 150;
 int blue = 0;
-bool MOVEMENT=false;
+bool MOVEMENT = false;
 bool connection = false;
 FirebaseData firebaseData;
 Timer modeRainbowTimer;
 Timer movementTimer;
+Timer colorCheckTimer;
+Timer modeCheckTimer;
+Timer alphaCheckTimer;
+Timer rainbowTimer;
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, ledPin, NEO_GRB + NEO_KHZ800);
 
-bool m = false;
+bool m = true;
+
+
+//////////////////////////////////
+/////// SETUP  ///////////////////
+//////////////////////////////////
 void setup() {
+  delay(2000);
   Serial.begin(115200); //enable Serial Monitor
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -44,86 +54,92 @@ void setup() {
 
   pinMode(pirPin, INPUT);   // pir pin
   pinMode(ledPin, OUTPUT);  // led pin
+  pinMode(BUILTIN_LED, OUTPUT);
+  digitalWrite(BUILTIN_LED, HIGH);
 
   strip.begin();
-  for (int ledNum = 0; ledNum < NUM_LEDS; ledNum++) {
-    strip.setPixelColor(ledNum, 0, 0, 0);
-  }
-  strip.show();
+  offMode();
 
   movementTimer.interval = 30000;
-  modeRainbowTimer.interval = 50;
+  modeCheckTimer.interval = 10000;
+  colorCheckTimer.interval = 10000;
+  alphaCheckTimer.interval = 10000;
+  rainbowTimer.interval = 100;
+
+  //ledSignal(3, 200);
 }
+
+//////////////////////////////////
+/////// LOOP  ////////////////////
+//////////////////////////////////
 
 void loop() {
   movementListener();
-  if (MOVEMENT) {
-    runLedStrip();
-  } else {
-    offLedStrip();
-  }
+  runLedStrip();
 }
 
+//////////////////////////////////
+/////// FUNCTIONS  ///////////////
+//////////////////////////////////
 void runLedStrip() {
+  if (MOVEMENT) {
+    if (ledStripMode.equals("color")) {
+      colorMode();
+    } else  if (ledStripMode.equals("rainbow")) {
+      rainbowMode();
+    } else  if (ledStripMode.equals("belarus")) {
+      belarusMode();
+    } else  if (ledStripMode.equals("auto")) {
+      randomMode();
+    } else  if (ledStripMode.equals("off")) {
+      offMode();
+    }
+  } else {
+    offMode();
+  }
+}
+
+void colorMode() {
+  colorValueListener();
   modeValueListener();
-  runLedStripMode();
-}
-
-void runLedStripMode() {
-  if (ledStripMode.equals("static")) {
-    staticMode();
-  } else  if (ledStripMode.equals("rainbow")) {
-    rainbowMode();
-  } else  if (ledStripMode.equals("b4b")) {
-    b4bMode();
-  }
-}
-
-void offLedStrip() {
-  for ( int ledNum = 0; ledNum < NUM_LEDS; ledNum++) {
-    strip.setPixelColor(ledNum, strip.Color(0, 0 , 0));
-  }
-  strip.show();
-}
-
-void staticMode() {
+  //ledSignal(2, 300);
   for (int i = 0; i < NUM_LEDS; i++) {
     strip.setPixelColor(i, strip.Color(red, green, blue));
   }
   strip.show();
-  colorValueListener();
-  modeValueListener();
 }
 
 void rainbowMode() {
-  for ( int i = 0; i < 255; i++) {
-    for ( int ledNum = 0; ledNum < NUM_LEDS; ledNum++) {
-      strip.setPixelColor(ledNum, strip.Color(red, i , blue));
+  uint16_t i, j;
+  for (j = 0; j < 256; j++) {
+    for (i = 0; i < NUM_LEDS; i++) {
+      strip.setPixelColor(i, Wheel((i * 1 + j) & 255));
     }
     strip.show();
-    delay(30);
-  }
-  for ( int i = 255; i > 0; i--) {
-    for ( int ledNum = 0; ledNum < NUM_LEDS; ledNum++) {
-      strip.setPixelColor(ledNum, strip.Color(red, i , blue));
-    }
-    delay(30);
-    strip.show();
+    delay(rainbowTimer.interval);
   }
   colorValueListener();
   modeValueListener();
-  /*
-    if (millis() - modeRainbowTimer.prev >= modeRainbowTimer.interval) {
-      modeRainbowTimer.prev = millis();   // "сбросить" таймер
-      strip.setPixelColor(pixNum, strip.Color(, 0, 0));
-      strip.show();
-    }
-  */
 }
 
-void b4bMode() {
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  if (WheelPos < 85) {
+    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  }  else if (WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }  else {
+    WheelPos -= 170;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+
+void belarusMode() {
   brightnessValueListener();
   modeValueListener();
+  //ledSignal(3, 300);
   for (int i = 0; i < NUM_LEDS; i++) {
     strip.setPixelColor(i, strip.Color(brightness, brightness, brightness));
   }
@@ -133,16 +149,34 @@ void b4bMode() {
   strip.show();
 }
 
+void randomMode(){
+  
+  }
+
 void modeValueListener() {
-  if (Firebase.getString(firebaseData, "panchenko/smallRoom/light/nightLight_1/ledStripMode")) {
-    ledStripMode = firebaseData.stringData();
+  if (millis() - modeCheckTimer.prev >= modeCheckTimer.interval) {
+    modeCheckTimer.prev = millis();
+    if (Firebase.getString(firebaseData, "panchenko/smallRoom/light/nightLight_1/ledStripMode")) {
+      ledStripMode = firebaseData.stringData();
+    }
+    do {
+      modeCheckTimer.prev += modeCheckTimer.interval;
+      if (modeCheckTimer.prev < modeCheckTimer.interval) break;  // переполнение uint32_t
+    } while (modeCheckTimer.prev < millis() - modeCheckTimer.interval); // защита от пропуска шага
   }
 }
 
 void colorValueListener() {
-  getRedColorFireBase();
-  getGreenColorFireBase();
-  getBlueColorFireBase();
+  if (millis() - colorCheckTimer.prev >= colorCheckTimer.interval) {
+    colorCheckTimer.prev = millis();
+    getRedColorFireBase();
+    getGreenColorFireBase();
+    getBlueColorFireBase();
+    do {
+      colorCheckTimer.prev += colorCheckTimer.interval;
+      if (colorCheckTimer.prev < colorCheckTimer.interval) break;  // переполнение uint32_t
+    } while (colorCheckTimer.prev < millis() - colorCheckTimer.interval); // защита от пропуска шага
+  }
 }
 
 void getRedColorFireBase() {
@@ -164,8 +198,15 @@ void  getBlueColorFireBase() {
 }
 
 void brightnessValueListener() {
-  if (Firebase.getInt(firebaseData, "panchenko/smallRoom/light/nightLight_1/color/alpha")) {
-    brightness = firebaseData.intData();
+  if (millis() - alphaCheckTimer.prev >= alphaCheckTimer.interval) {
+    alphaCheckTimer.prev = millis();
+    if (Firebase.getInt(firebaseData, "panchenko/smallRoom/light/nightLight_1/color/alpha")) {
+      brightness = firebaseData.intData();
+    }
+    do {
+      alphaCheckTimer.prev += alphaCheckTimer.interval;
+      if (alphaCheckTimer.prev < alphaCheckTimer.interval) break;  // переполнение uint32_t
+    } while (alphaCheckTimer.prev < millis() - alphaCheckTimer.interval); // защита от пропуска шага
   }
 }
 
@@ -174,19 +215,37 @@ void movementListener() {
     Serial.println("move");
     MOVEMENT = true;
     movementTimer.prev = millis();
-  }
-  if (millis() - movementTimer.prev >= movementTimer.interval) {
-    Serial.println("not move");
-    MOVEMENT = false;
+  } else {
+    if (millis() - movementTimer.prev >= movementTimer.interval) {
+      Serial.println("not move");
+      MOVEMENT = false;
+      do {
+        movementTimer.prev += movementTimer.interval;
+        if (movementTimer.prev < movementTimer.interval) break;  // переполнение uint32_t
+      } while (movementTimer.prev < millis() - movementTimer.interval); // защита от пропуска шага
+    }
   }
 }
 
 bool isMovement(int pin) {
-  bool m;
-  if (digitalRead(pin) == HIGH) {
-    m = true;
-  } else if (digitalRead(pin) == LOW) {
-    m = false;
+  return digitalRead(pin) == HIGH ? true : false;
+}
+
+void offMode() {
+  for (int ledNum = 0; ledNum < NUM_LEDS; ledNum++) {
+    strip.setPixelColor(ledNum, 0, 0, 0);
   }
-  return m;
+  strip.show();
+  //digitalWrite(BUILTIN_LED, HIGH); // led off
+  modeValueListener();
+}
+
+void ledSignal(int count, int milliseconds) {
+  for (int i = 0; i < count; i++) {
+    digitalWrite(BUILTIN_LED, LOW);
+    delay(milliseconds);
+    digitalWrite(BUILTIN_LED, HIGH);
+    delay(milliseconds);
+  }
+  delay(milliseconds);
 }
